4a74862410cfcb82fd9ad1729f35046face8671c
Angger-Raka <78834298+Angger-Raka@users.noreply.github.com>
2021-02-23 11:41:31 +0700

    mwencoba

---
+++ YEET.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('Listening.....')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
